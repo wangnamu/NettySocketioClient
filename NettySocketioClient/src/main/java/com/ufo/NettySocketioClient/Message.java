4c0d3f9afe7d479c88d6517bc41ec14eb4940258
wangnamu <wangstin@sina.com>
2017-02-07 10:09:25 +0800

    2017 first code

--- NettySocketioClient/src/main/java/com/ufo/NettySocketioClient/Message.java
+++ NettySocketioClient/src/main/java/com/ufo/NettySocketioClient/Message.java
@@ -3,85 +3,87 @@ package com.ufo.NettySocketioClient;
 import java.util.HashSet;
 
 public class Message {
+	
 	// 主键
-	private String SID;
-	// 发送人ID
-	private String SenderID;
-	// 接收人ID
-	private HashSet<String> ReceiverIDs;
-	// 标题
-	private String Alert;
-	// 时间
-	private long Time;
-	// 消息类型(文字、图片、文件、链接、音频、视频、表情等)
-	private String MessageType;
-	// 提醒
-	private Boolean IsAlert;
-	// 自定义
-	private Object Others;
-
-	public String getSID() {
-		return SID;
-	}
-
-	public void setSID(String sID) {
-		SID = sID;
-	}
-
-	public String getSenderID() {
-		return SenderID;
-	}
-
-	public void setSenderID(String senderID) {
-		SenderID = senderID;
-	}
-
-	public HashSet<String> getReceiverIDs() {
-		return ReceiverIDs;
-	}
-
-	public void setReceiverIDs(HashSet<String> receiverIDs) {
-		ReceiverIDs = receiverIDs;
-	}
-
-	public String getAlert() {
-		return Alert;
-	}
-
-	public void setAlert(String alert) {
-		Alert = alert;
-	}
-
-	public long getTime() {
-		return Time;
-	}
-
-	public void setTime(long time) {
-		Time = time;
-	}
-
-
-	public String getMessageType() {
-		return MessageType;
-	}
-
-	public void setMessageType(String messageType) {
-		MessageType = messageType;
-	}
-
-	public Boolean getIsAlert() {
-		return IsAlert;
-	}
-
-	public void setIsAlert(Boolean isAlert) {
-		IsAlert = isAlert;
-	}
-
-	public Object getOthers() {
-		return Others;
-	}
-
-	public void setOthers(Object others) {
-		Others = others;
-	}
+		private String SID;
+		// 发送人ID
+		private String SenderID;
+		// 接收人ID
+		private HashSet<String> ReceiverIDs;
+		// 标题
+		private String Title;
+		//内容
+		private String Body;
+		// 时间
+		private long Time;
+		// 消息类型(文字、图片、文件、链接、音频、视频、表情等)
+		private String MessageType;
+		// 提醒
+		private Boolean IsAlert;
+		// 针对ios10和androidN的快捷回复功能，Category字段表示快捷方式分类
+		private String Category;
+		// 自定义
+		private Object Others;
+		
+		public String getSID() {
+			return SID;
+		}
+		public void setSID(String sID) {
+			SID = sID;
+		}
+		public String getSenderID() {
+			return SenderID;
+		}
+		public void setSenderID(String senderID) {
+			SenderID = senderID;
+		}
+		public HashSet<String> getReceiverIDs() {
+			return ReceiverIDs;
+		}
+		public void setReceiverIDs(HashSet<String> receiverIDs) {
+			ReceiverIDs = receiverIDs;
+		}
+		public String getTitle() {
+			return Title;
+		}
+		public void setTitle(String title) {
+			Title = title;
+		}
+		public String getBody() {
+			return Body;
+		}
+		public void setBody(String body) {
+			Body = body;
+		}
+		public long getTime() {
+			return Time;
+		}
+		public void setTime(long time) {
+			Time = time;
+		}
+		public String getMessageType() {
+			return MessageType;
+		}
+		public void setMessageType(String messageType) {
+			MessageType = messageType;
+		}
+		public Boolean getIsAlert() {
+			return IsAlert;
+		}
+		public void setIsAlert(Boolean isAlert) {
+			IsAlert = isAlert;
+		}
+		public String getCategory() {
+			return Category;
+		}
+		public void setCategory(String category) {
+			Category = category;
+		}
+		public Object getOthers() {
+			return Others;
+		}
+		public void setOthers(Object others) {
+			Others = others;
+		}
+	
 }
--- NettySocketioClient/src/main/java/com/ufo/NettySocketioClient/NettySocketioClient.java
+++ NettySocketioClient/src/main/java/com/ufo/NettySocketioClient/NettySocketioClient.java
@@ -1,7 +1,6 @@
 package com.ufo.NettySocketioClient;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
@@ -11,40 +10,44 @@ import com.google.gson.Gson;
 
 public class NettySocketioClient {
 
-	private final static String SEND = "send";
+	private final static String NEWS = "news";
 	private final static String BROADCAST = "broadcast";
+	private final static String NOTIFYOTHERPLATFORMS = "notifyotherplatforms";
 
 	private static NettySocketioClient INSTANCE = null;
 	private RedissonClient mRedisson;
 
-	private NettySocketioClient() {
-		try {
-			String path = this.getClass().getResource("/").getPath().toString() + "redis-config.json";
-			Config redisConfig = Config.fromJSON(new File(path));
-			mRedisson = Redisson.create(redisConfig);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	private NettySocketioClient() throws Exception {
+
+		String path = this.getClass().getResource("/").getPath().toString() + "redis-config.json";
+		Config redisConfig = Config.fromJSON(new File(path));
+		mRedisson = Redisson.create(redisConfig);
+
 	}
 
-	public static NettySocketioClient getInstance() {
+	public static NettySocketioClient getInstance() throws Exception {
 		if (INSTANCE == null) {
 			INSTANCE = new NettySocketioClient();
 		}
 		return INSTANCE;
 	}
 
-	public long send(Message message) {
+	public long sendNews(Message message) {
 		Gson gson = new Gson();
 		String json = gson.toJson(message);
-		return mRedisson.getTopic(SEND).publish(json);
+		return mRedisson.getTopic(NEWS).publish(json);
 	}
 
 	public long broadcast(Message message) {
 		Gson gson = new Gson();
 		String json = gson.toJson(message);
 		return mRedisson.getTopic(BROADCAST).publish(json);
+	}
+
+	public long notifyOtherPlatforms(Notify notify) {
+		Gson gson = new Gson();
+		String json = gson.toJson(notify);
+		return mRedisson.getTopic(NOTIFYOTHERPLATFORMS).publish(json);
 	}
 
 }
